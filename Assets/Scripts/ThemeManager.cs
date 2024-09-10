0c2ad9327331aebb90ec5623a205d96947d1e824
Volod <volodiabussereau@gmail.com>
2024-09-10 16:51:10 +0200

    UI system start and PlayerControls InputSystem

--- Assets/Scripts/ThemeManager.cs
+++ Assets/Scripts/ThemeManager.cs
@@ -6,13 +6,13 @@ using Random = UnityEngine.Random;
 
 public class ThemeManager : MonoBehaviour
 {
-    [SerializeField] private List<JokeTheme> _jokeThemes;
-    private List<JokeTheme> _currJokeThemes;
+    [SerializeField] private List<string> _jokeThemes;
+    private List<string> _currJokeThemes;
 
     private void Awake()
     {
-        _currJokeThemes = new List<JokeTheme>();
-        foreach (JokeTheme jTheme in _jokeThemes)
+        _currJokeThemes = new List<string>();
+        foreach (string jTheme in _jokeThemes)
         {
             _currJokeThemes.Add(jTheme);
         }
@@ -21,31 +21,21 @@ public class ThemeManager : MonoBehaviour
     [Button]
     private void GetThemeTest()
     {
-        JokeTheme testTheme = GetTheme();
-        Debug.Log("theme: " + testTheme.theme + ", multi: " + testTheme.multiplicator);
+        string testTheme = GetTheme();
+        Debug.Log("theme: " + testTheme);
     }
     
-    private JokeTheme GetTheme()
+    public string GetTheme()
     {
-        JokeTheme jokeTheme = _currJokeThemes[Random.Range(0, _currJokeThemes.Count)];
+        string jokeTheme = _currJokeThemes[Random.Range(0, _currJokeThemes.Count)];
         _currJokeThemes.Remove(jokeTheme);
         if (_currJokeThemes.Count <= 0)
         {
-            foreach (JokeTheme jTheme in _jokeThemes)
+            foreach (string jTheme in _jokeThemes)
             {
                 _currJokeThemes.Add(jTheme);
             }
         }
         return jokeTheme;
     }
-}
-
-[Serializable]
-public struct JokeTheme
-{
-    [SerializeField] public string theme;
-    [SerializeField] public int multiplicator;
-
-    public string Theme => theme;
-    public int Multiplicator => multiplicator;
 }
